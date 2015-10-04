669bd35e357be2025f4429d2f581828569219ccc
jairxserfer <jairxserfer@hotmail.com>
2015-10-04 18:47:55 -0500

    initial commit

---
+++ urionlinejudge/1238.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<char>  combiner(string a, string b){
+  int buffer1 = a.size();
+  int buffer2 = b.size();
+  int maxB;
+  vector<char> result;
+  if(buffer1 > buffer2)
+    maxB = buffer1;
+  else
+    maxB = buffer2;
+
+  for( int i = 0; i < maxB; i++ )
+  {
+    if(maxB == buffer1)
+    {
+      result.push_back(a[i]);
+      if(buffer2 <= i)
+        result.push_back(b[i]);
+    } else{
+      result.push_back(a[i]);
+      if(buffer1 <= i)
+        result.push_back(b[i]);
+    }
+  }
+  return result;
+}
+
+
+int main(){
+  long  long a;
+  cin >> a;
+  while(a--){
+    string a, b;
+    cin >> a >> b;
+    vector<char> resultFinal = combiner(a,b);
+    for(int i = 0; i < resultFinal.size(); i++)
+      cout << resultFinal[i];
+    cout << endl;
+  }
+  return 0;
+}
