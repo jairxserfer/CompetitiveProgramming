3da03befe53546c85a627c5d3f511a7584d34b64
jairxserfer <jairxserfer@hotmail.com>
2016-02-12 20:22:55 -0500

    initial commit

--- Estructura de Datos/colas.cpp
+++ Estructura de Datos/colas.cpp
@@ -14,15 +14,44 @@ typedef nodo *Tcolas;
 */
 
 //Ingresar algun elemento a la cola
-void encolar(Tnodo &Tcolas, int aInsertar)
+void encolar(Tcolas &Colas, int aInsertar)
 {
   Tnodo q = new(struct nodo), m;
   q -> dato = aInsertar;
   q -> sgte = NULL;
-
+  if(Colas == NULL)
+  {
+    Colas = q;
+  } else
+  {
+    m = Colas;
+    while (m -> sgte != NULL)
+    {
+      m = m -> sgte;
+    }
+    m -> sgte = q;
+  }
 }
 
+//Eliminar (hacer correr) a algun elemneto de la cola
+void desencolar(Tcolas &Cola, int aInsertar)
+{
+  Tcolas q = Cola;
+  Cola = Cola->sgte;
+  delete(q);
+}
 
+//Solo muestra lo que hay en la cola
+
+void mostrar(Tcolas Cola) {
+  int i = 1;
+  while(Cola != NULL)
+  {
+    cout << "El elemento: " << i <<" es "<< Cola-> dato <<"\n";
+    Cola = Cola -> sgte;
+    i++;
+  }
+}
 
 int main(int argc, char const *argv[]) {
   /* code */
